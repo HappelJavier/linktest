91091047a946f55a9b2b8c2e5e57862ddd1f6664
JH_E91232 <JH_E91232@LNKUSR1884.redlink.com.ar>
2022-04-24 12:33:01 -0300

    "test commit"

---
+++ src/main/java/UTN/practicas/inscripciones/App.java
@@ -0,0 +1,13 @@
+package UTN.practicas.inscripciones;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/UTN/practicas/inscripciones/Inscripciones.java
@@ -0,0 +1,9 @@
+package UTN.practicas.inscripciones;
+
+import java.util.ArrayList;
+
+public class Inscripciones {
+	Alumno javier = new Alumno("Javier", new String[]{"algoritmos","arquitectura"});
+	
+
+}
